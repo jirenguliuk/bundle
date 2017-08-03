38872a1ac0e85fa81f23cb7ecda32a2a109436c9
jirenguliuk <707529659@qq.com>
2017-08-03 23:47:52 +0800

    third

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,19 @@
-var path = require('path');
+var webpack = require('webpack')
+var path = require("path")
 
 module.exports = {
   entry: './app/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
-  }
-};
+  },
+  
+ 
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
+    })
+  ]
+}
